248f723d281ff3dfb2f20ebc5a95c49eaca99e7c
Antonenko Olexiy <antonenkoolexiy.com>
2021-03-05 00:10:43 +0200

    init rep

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1. Define an amount of digits in number:
+
+// const defineAmount = (num) => {
+//     let c = 1;
+//     while ( ( num /= 10) >= 1 )
+//         c++;
+//     console.log(c);
+// }
+
+// defineAmount(100)
+
+//############################################
+
+//2. Define number to the certain power:
+
+// const definePow = (a, n) => {
+    
+//     console.log(Math.pow(a, n));
+// }
+
+// definePow(88, 0)
+
+//##############################################
+
+//3. Change number in array:
+
+// const someArray = [1, 3, 5, 8, 13, 21]
+
+// const changeNumber = (someArray, p, k) => {
+//     someArray.splice(k+1, 1, p)
+//     console.log(someArray);
+// }
+
+// changeNumber(someArray, 77, 77)
